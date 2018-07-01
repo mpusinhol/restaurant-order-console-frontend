80ab9b1d43229f992b5d9b960dee2a752361657f
Matheus Pusinhol <matheus@dittomusic.com>
2018-07-01 20:58:09 -0300

    Remove redux from index

--- src/index.js
+++ src/index.js
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 
 import App from './app/index';
-import reducers from './modules/index';
 
-const store = createStore(reducers);
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById('app'));
